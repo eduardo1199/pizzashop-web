98a53e0a7613d8f2a3b78f72f5e770e5e04d7efb
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-02-03 10:45:54 -0300

    setting project pizza shop

---
+++ src/routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './pages/layouts/app'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
+])
