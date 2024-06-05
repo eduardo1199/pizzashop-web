28f25195196dc1ae73ec717951a7b1506803bdf6
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-06-05 18:53:07 -0300

    feat: create setup test e2e with playwright

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './test',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3333',
+  },
+  webServer: {
+    command: 'npm run dev:test',
+    url: 'http://localhost:3333',
+    reuseExistingServer: !process.env.CI,
+  },
+
+  /* Configure projects for major browsers */
+  /* projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ], */
+})
